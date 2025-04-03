a92fe8ef029b42c364cfcf94a53b1fa9ab8bc467
Kenneth Loenbaek <kenloen@dtu.dk>
2025-04-03 09:15:31 +0200

    Attempt to fix google analytics warning: https://mtakagishi.com/en/tools/sphinx/google_analytics_tag#id1

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,8 @@ html_static_path = ['_static']
 #
 html_theme = 'pydata_sphinx_theme'
 
-html_theme_options = {
+html_theme_options = {}
+html_theme_options["analytics"] = {
     "google_analytics_id": "G-8GPVFR9N4C",
 }
 
