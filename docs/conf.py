544620bc5b33828f6aa66f7e70a9a372e25fd78d
fzahle <frza@dtu.dk>
2025-06-04 14:04:17 +0200

    run the schema_export.py script from sphinx, which should ensure that the multiversion package will render the correct schema now that we dont track it in git

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,83 @@ def on_config_inited(app, config):
         "version_match": version_match
     }
 
+def schema_export(app):
+    #  This function will be called after a document is processed
+
+
+
+    version = getattr(app.config, "smv_current_version")
+
+    # Skip schema export for version 1.0
+    if version == "1.0":
+        return
+
+    # run the schema_export script in a subprocess, since we otherwise
+    # for some reason pick up the windIO/yaml.py file when
+    # json_schema_for_humans imports yaml.
+    import subprocess
+    import os
+
+    env = os.environ.copy()
+
+    subprocess.run(["python", "schema_export.py"], env=env)
+    return
+
+
+    from pathlib import Path
+
+    from json_schema_for_humans.generate import generate_from_filename
+    from json_schema_for_humans.generation_configuration import GenerationConfiguration
+
+
+
+    # Configure the docs
+    config = GenerationConfiguration(
+        minify=False,
+        copy_css=True,
+        copy_js=True,
+        expand_buttons=True,
+        show_breadcrumbs=False,     # True doesn't seem to work
+        show_toc=True,
+        collapse_long_descriptions=True,
+        collapse_long_examples=True,
+        description_is_markdown=True,
+        examples_as_yaml=True,
+        link_to_reused_ref=True,    # Should we do this or duplicate the entry?
+        deprecated_from_description=True,
+        template_md_options={
+        #     "badge_as_image": True,
+            "show_heading_numbers": True
+        },
+        # template_name="md_nested"
+        custom_template_path="jsfh_template/base.html"
+    )
+
+    config.schema_title=False
+
+    # Using the json file and config from above, create the docs web page
+    base_path = Path(os.getcwd()).parent
+
+    # Generate the plant schema html
+
+    schema_html_path = Path("_static/plant_schema.html")
+    generate_from_filename(
+        base_path / "windIO" / "schemas" / "plant" / "wind_energy_system.yaml",
+        schema_html_path,
+        config=config
+    )
+
+    # Generate the turbine schema html
+    schema_html_path = Path("_static/turbine_schema.html")
+    generate_from_filename(
+        base_path / "windIO" / "schemas" / "turbine" / "turbine_schema.yaml",
+        schema_html_path,
+        config=config
+    )
+
+
+
 def setup(app):
     # Connect our custom handler to the config-inited event
     app.connect("config-inited", on_config_inited)
+    app.connect('builder-inited', schema_export)
